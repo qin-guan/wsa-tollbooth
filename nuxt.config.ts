22f7f2a2d14551443ef117fed05a890e4b9bdd0f
Qin Guan <qinguan20040914@gmail.com>
2023-07-17 21:55:11 +0800

    feat: test swr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
     '/dashboard/surveys/*/print': { ssr: false },
+    '/s/*': { swr: 120 },
+    '/s/*/analytics': { swr: 30 },
   },
   modules: [
     '@unocss/nuxt',
